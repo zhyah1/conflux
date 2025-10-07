d320bd427f7a014978cb02b837d0c86cde06f76c
projectr2g <projectr2gen@gmail.com>
2025-10-07 07:30:00 +0000

    now the add project button and grantt chart button alognment is changed

--- src/app/dashboard/components/page-header.tsx
+++ src/app/dashboard/components/page-header.tsx
@@ -8,14 +8,12 @@ type PageHeaderProps = {
 
 export function PageHeader({ title, description, children }: PageHeaderProps) {
   return (
-    <div className="flex items-start justify-between space-y-2 mb-6">
-      <div className="flex items-center gap-4">
-        {children && <div className="flex items-center space-x-2">{children}</div>}
-        <div>
-            <h1 className="text-3xl font-bold tracking-tight font-headline">{title}</h1>
-            {description && <div className="text-muted-foreground">{description}</div>}
-        </div>
+    <div className="flex items-start justify-between">
+      <div className="flex-1">
+        <h1 className="text-3xl font-bold tracking-tight font-headline">{title}</h1>
+        {description && <div className="text-muted-foreground">{description}</div>}
       </div>
+      {children && <div className="flex items-center gap-2">{children}</div>}
     </div>
   );
 }
